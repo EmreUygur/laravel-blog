e720da5514a51ca85247eddd2bda6f91b7017868
Emre UYGUR <60958122+dogo-emre@users.noreply.github.com>
2020-03-28 20:01:19 +0300

    Creating Models & Migrations For Database

---
+++ database/migrations/2020_03_27_134655_create_jobs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJobsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('company');
+            $table->string('position');
+            $table->string('reference_name');
+            $table->string('reference_phone');
+            $table->text('note')->nullable();
+            $table->date('started_at');
+            $table->date('quitted_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jobs');
+    }
+}
---
+++ database/migrations/2020_03_27_135537_create_education_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEducationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('education', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('school_name');
+            $table->string('department_name');
+            $table->string('gpa')->nullable();
+            $table->text('note')->nullable();
+            $table->date('started_at');
+            $table->date('finished_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('education');
+    }
+}
