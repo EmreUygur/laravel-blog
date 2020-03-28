8ff468d95a49f182c9f850279774638f8a1ff217
Emre UYGUR <60958122+dogo-emre@users.noreply.github.com>
2020-03-28 20:06:18 +0300

    Changing layouts

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,9 +1,20 @@
-@extends('layouts/app')
+@extends('layouts/main')
 
-@section('title')
+@section('layoutTitle')
     Contact
 @endsection
 
-@section('content')
-    <div class="text-2xl font-semibold text-gray-700">Contact Me</div> 
+@section('layoutContent')
+    <div class="text-2xl font-semibold text-gray-700">Contact Me</div>
+    <div class="text-lg mt-2 font-medium text-gray-700">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae quod, repellat et numquam reiciendis inventore nobis temporibus tempore vel dignissimos corrupti deleniti perspiciatis, dolore ab! Hic cum possimus totam nulla!
+    </div>
+    <div class="flex flex-wrap justify-center mt-8">
+        <div class="w-full sm:w-1/2">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil, veniam numquam voluptatem corporis, tenetur nobis quasi corrupti tempore rem repellat ducimus! Eos totam corrupti quia quidem ut? Aut, assumenda qui!
+        </div>
+        <div class="w-full sm:w-1/2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, alias est nisi illum laudantium, quam totam iure doloremque non temporibus incidunt repudiandae officiis tempora quis ipsam possimus. Quis, alias quisquam.
+        </div>
+    </div>
 @endsection
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,10 +1,11 @@
-@extends('layouts/app')
+@extends('layouts/main')
 
-@section('title')
+@section('layoutTitile')
     Home
 @endsection
 
-@section('content')
+@section('layoutContent')
     <img id="bg-homepage" src="{{ asset('/images/programming.gif') }}" alt="programming">
     <div class="text-2xl text-gray-700 font-semibold">Emre UYGUR</div>
+    <div class="mt-1 text-lg text-gray-700 font-semibold">Computer Engineer</div>
 @endsection
