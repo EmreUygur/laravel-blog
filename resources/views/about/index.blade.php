5ef1d0eaa0b48a3e5b164523ef05907f292195f4
Emre UYGUR <60958122+dogo-emre@users.noreply.github.com>
2020-04-16 20:41:14 +0300

    About page Updated

--- resources/views/about/index.blade.php
+++ resources/views/about/index.blade.php
@@ -13,8 +13,7 @@
                     @if ($biography)
                         {{ $biography->biography }}
                     @else
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur accusamus fugiat explicabo. Dolores error unde porro aut, aspernatur quam consequatur, perspiciatis corporis eveniet, rerum eos vero ad nesciunt maiores nobis!
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, placeat veniam voluptates repellendus accusantium tenetur dolor magnam est soluta corrupti. Vitae modi fugiat sunt nulla officiis est, ipsa magni accusamus.
+                        Biography is not entered yet...
                     @endif
                 </div>
             </div>
@@ -24,23 +23,17 @@
                 <div class="p-4 text-gray-700 rounded bg-white h-full">
                     <div class="text-2xl font-semibold select-none mb-2">Job Experiences</div>
                     <div class="jobs-list flex flex-col">
-                        <div class="job flex-flex-col">
-                            <div class="company-title text-xl font-medium">DOGO Tasarım</div>
-                            <div class="font-normal text-gray-600 text-lg px-1">Front-end Developer</div>
-                            <div class="font-normal text-gray-600 text-md px-1">02/2020 - 03/2020</div>
-                        </div>
-
-                        <div class="job flex-flex-col">
-                            <div class="company-title text-xl font-medium">Lazermarket</div>
-                            <div class="font-normal text-gray-600 text-lg px-1">Internship</div>
-                            <div class="font-normal text-gray-600 text-md px-1">07/2018 - 08/2018</div>
-                        </div>
-
-                        <div class="job flex-flex-col">
-                            <div class="company-title text-xl font-medium">Ankakep Bilişim</div>
-                            <div class="texttext-gray-600 text-lg px-1">Internship</div>
-                            <div class="text-gray-600 text-md px-1">06/2016 - 08/2016</div>
-                        </div>
+                        @if (count($jobs))
+                            @foreach ($jobs as $job)
+                                <div class="job flex-flex-col">
+                                    <div class="company-title text-xl font-medium">{{ $job->company }}</div>
+                                    <div class="font-normal text-gray-600 text-lg px-1">{{ $job->position }}</div>
+                                    <div class="font-normal text-gray-600 text-md px-1">{{ $job->started_at }} - {{ $job->quitted_at }}</div>
+                                </div>
+                            @endforeach
+                        @else 
+                            No working experience information entered yet...
+                        @endif
                     </div>                
                 </div>
             </div>
@@ -48,19 +41,23 @@
                 <div class="p-4 text-gray-700 rounded bg-white h-full">
                     <div class="text-2xl font-semibold select-none">Education</div>
                     <div class="educations-list flex flex-col">
-                        <div class="school flex-flex-col">
-                            <div class="school-title text-xl font-medium">Celal Bayar University</div>
-                            <div class="text-gray-600 text-lg px-1">Computer Engineering</div>
-                            <div class="text-gray-600 text-md px-1">09/2013 - 08/2019</div>
-                            <div class="gda text-gray-600 text-sm px-1">2.97/4</div>
-                        </div>
-
-                        <div class="school flex-flex-col">
-                            <div class="school-title text-xl font-medium">Polytechnic Institute of Coimbra</div>
-                            <div class="text-gray-600 text-lg px-1">Computer Engineering</div>
-                            <div class="text-gray-600 text-md px-1">09/2016 - 08/2017</div>
-                            <div class="note text-gray-600 text-sm px-1">Erasmus Student Mobility</div>
-                        </div>
+                        @if (count($educations))
+                            @foreach ($educations as $education)
+                                <div class="school flex-flex-col">
+                                    <div class="school-title text-xl font-medium">{{ $education->school_name }}</div>
+                                    <div class="text-gray-600 text-lg px-1">{{ $education->department_name }}</div>
+                                    <div class="text-gray-600 text-md px-1">{{ $education->started_at }} - {{ $education->finished_at }}</div>
+                                    @if($education->gpa)
+                                        <div class="gda text-gray-600 text-sm px-1">{{ $education->gpa }}</div>
+                                    @endif
+                                    @if($education->note)
+                                        <div class="gda text-gray-600 text-sm px-1">{{ $education->note }}</div>
+                                    @endif
+                                </div>
+                            @endforeach
+                        @else 
+                            No educational information entered yet...
+                        @endif
                     </div>                         
                 </div>
             </div>
