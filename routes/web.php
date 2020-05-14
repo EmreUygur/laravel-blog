cd9b7da718106f2b7aa8482d3214ba1a099b2523
Emre UYGUR <60958122+dogo-emre@users.noreply.github.com>
2020-05-14 16:48:33 +0300

    Update web.php
    Auth routes initialized

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Auth::routes();
 
 Route::get('/', 'MainController@home');
 Route::get('/about', 'MainController@about');
