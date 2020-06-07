58b7abd0a894035f416a8e25c5278d2e0b60ffba
Emre UYGUR <60958122+dogo-emre@users.noreply.github.com>
2020-06-07 23:41:07 +0300

    Update create.blade.php
    Validation errors now can be seen by user

--- resources/views/blog/create.blade.php
+++ resources/views/blog/create.blade.php
@@ -11,33 +11,52 @@
         <div class="flex flex-col mt-6">
             <div class="m-2 flex flex-col">
                 <label for="title">Title</label>
-                <input type="text" class="border rounded border-gray-200 p-1 focus:border-blue-200" id="title" name="title"/>
+                <input type="text" value="{{ old("title") }}" class="border rounded border-gray-200 p-1 focus:border-blue-200" id="title" name="title"/>
+                @error("title")
+                    <p class="text-sm text-red-500 font-semibold">
+                        {{ $errors->first("title") }}
+                    </p>
+                @enderror
             </div>
             <div class="m-2 flex flex-col">
                 <label for="excerpt">Excerpt</label>
-                <input type="text" class="border rounded border-gray-200 p-1 focus:border-blue-200" id="excerpt" name="excerpt" />
+                <input type="text" value="{{ old("excerpt") }}" class="border rounded border-gray-200 p-1 focus:border-blue-200" id="excerpt" name="excerpt" />
+                @error("excerpt")
+                    <p class="text-sm text-red-500 font-semibold">
+                        {{ $errors->first("excerpt") }}
+                    </p>
+                @enderror
             </div>
             <div class="m-2 flex flex-col">
                 <label for="body">Body</label>
-                <editor></editor>
+                <editor body="{!! old("body") !!}"></editor>
+                @error("body")
+                    <p class="text-sm text-red-500 font-semibold">
+                        {{ $errors->first("body") }}
+                    </p>
+                @enderror
             </div>
             <div class="m-2 flex flex-col">
                 <label for="cover_image">Cover Image</label>
                 <input type="file" name="cover_image" id="cover_image">
+                @error("cover_image")
+                    <p class="text-sm text-red-500 font-semibold">
+                        {{ $errors->first("cover_image") }}
+                    </p>
+                @enderror
             </div>
             <div class="m-2 flex flex-col">
                 <label for="tags[]">Tags</label>
-                {{-- <select id="tags[]" name="tags[]" class="border rounded border-gray-200 p-1 focus:border-blue-200 h-20" multiple>
-                    @foreach ($tags as $tag)
-                        <option value="{{ $tag->id }}">{{ $tag->name }}</option>
-                    @endforeach
-                </select> --}}
-                <tags-input :all="{{ json_encode($tags) }}" :selected="[]" ></tags-input>
+                <tags-input :all="{{ json_encode($tags) }}" ></tags-input>
+                @error("tags")
+                    <p class="text-sm text-red-500 font-semibold">
+                        {{ $errors->first("tags") }}
+                    </p>
+                @enderror
             </div>
             <div class="m-2">
                 <button type="submit" class="px-2 py-1 bg-blue-600 text-white rounded focus:border-blue-200">Submit</button>
             </div>
         </div>
     </form>
-    {{-- <tags-input :existedTags="{{ json_encode($tags) }}" :tags="[]" name="tags[]" ></tags-input> --}}
 @endsection
