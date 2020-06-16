e844a9cd93aa00cb6155ea09537821fbfd04354d
Emre UYGUR <60958122+dogo-emre@users.noreply.github.com>
2020-06-16 14:40:26 +0300

    Update index.blade.php

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -5,9 +5,9 @@
 @endsection
 
 @section('layoutContent')
-    <div class="flex flex-wrap sm:mx-16 items-start">
-        <div class="flex flex-col w-full sm:w-4/5">
-            @if(count($articles))
+    @if(count($articles))
+        <div class="flex flex-wrap sm:mx-16 items-start">
+            <div class="flex flex-col w-full sm:w-4/5">
                 @foreach ($articles as $article)
                     <div class="flex flex-col mb-8">
                         <div class="text-3xl text-gray-700 font-semibold">
@@ -23,26 +23,26 @@
                         </div>
                     </div>
                 @endforeach
-            @else 
-                <div class="mt-16 text-3xl text-red-400 font-bold text-center">
-                    No articles entered yet :/
+            </div>
+            <div class="tagsCard flex flex-col w-full sm:w-1/5 mt-4 sm:mt-0 rounded">
+                <div class="tagsHeader p-2 mb-2">
+                    Tags
                 </div>
-            @endif
-        </div>
-        <div class="tagsCard flex flex-col w-full sm:w-1/5 mt-4 sm:mt-0 rounded">
-            <div class="tagsHeader p-2 mb-2">
-                Tags
-            </div>
-            <div class="flex flex-wrap m-2">
-                <a href="/blog" class="bg-tag py-1 px-2 rounded text-sm mr-2 mb-2">All</a>
-                @if(count($tags))
-                    @foreach ( $tags as $tag )
-                        <a href="/blog?tag={{ $tag->name }}" class="bg-tag py-1 px-2 rounded text-sm mr-2 mb-2"> {{ $tag->name }}</a>
-                    @endforeach
-                @endif
+                <div class="flex flex-wrap m-2">
+                    <a href="/blog" class="bg-tag py-1 px-2 rounded text-sm mr-2 mb-2">All</a>
+                    @if(count($tags))
+                        @foreach ( $tags as $tag )
+                            <a href="/blog?tag={{ $tag->name }}" class="bg-tag py-1 px-2 rounded text-sm mr-2 mb-2"> {{ $tag->name }}</a>
+                        @endforeach
+                    @endif
+                </div>
             </div>
         </div>
-    </div>
+    @else 
+        <div class="p-16 text-3xl text-red-400 font-bold text-center">
+            No articles entered yet :/
+        </div>
+    @endif
     @auth
         <a href="/blog/articles/create" class="rounded-full h-12 w-12 m-2 bottom-0 right-0 fixed flex items-center justify-center bg-green-500 text-white">
             <i class="fas fa-plus"></i>
