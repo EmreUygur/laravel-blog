119c909fab63446cf592660b0f91c85b6f837420
Emre UYGUR <60958122+dogo-emre@users.noreply.github.com>
2020-06-16 14:40:23 +0300

    Update index.blade.php

--- resources/views/about/index.blade.php
+++ resources/views/about/index.blade.php
@@ -7,16 +7,28 @@
 @section('layoutContent')
     <div class="flex flex-wrap justify-center items-stretch z-10">
         <div class="w-full pb-4 lg:w-1/3 lg:pr-4 lg:pb-0">
-            <div class="p-4 text-gray-700 w-full h-full rounded bg-white">
+            <div class="p-4 text-gray-700 w-full h-full rounded bg-white relative">
                 <div class="flex justify-start items-end mb-3">
                     <img class="w-1/4 rounded-full mr-2" draggable="false" src="{{ asset('images/avatar.jpg') }}" alt="avatar">
-                    <div class="text-2xl font-semibold select-none">>_whoami</div>
+                    <div class="text-2xl font-semibold select-none"><i class="fas fa-terminal"></i> whoami</div>
                 </div>
                 <div>
                     @if ($biography)
                         {!! $biography->biography !!}
+                        @auth
+                            <div class="mt-2 py-2 px-4 border-t border-gray-200 absolute inset-x-0 bottom-0">
+                                <a href="/about/biography/edit" class="text-sm text-blue-400 hover:underline">Edit</a>
+                            </div>
+                        @endauth
                     @else
-                        Biography is not entered yet...
+                        <div class="h-16">
+                            Biography is not entered yet...
+                        </div>
+                        @auth
+                            <div class="mt-2 py-2 px-4 border-t border-gray-200 absolute inset-x-0 bottom-0">
+                                <a href="/about/biography/create" class="text-sm text-blue-400 hover:underline">Create your biography</a>
+                            </div>
+                        @endauth
                     @endif
                 </div>
             </div>
@@ -24,7 +36,16 @@
         <div class="w-full lg:w-2/3 lg:pl-4 flex flex-col">
             <div class="pb-2 h-full">
                 <div class="p-4 text-gray-700 rounded bg-white h-full">
-                    <div class="text-2xl font-semibold select-none mb-2">Job Experiences</div>
+                    <div class="flex select-none mb-2 flex items-center justify-between">
+                        <div class="text-2xl font-semibold ">
+                            <i class="fas fa-briefcase"></i> Job Experiences
+                        </div>
+                        @auth
+                            <div class="text-sm">
+                                <a href="/about/job/create" class="text-sm text-blue-400 hover:underline">Add</a>
+                            </div>
+                        @endauth
+                    </div>
                     <div class="jobs-list flex flex-col">
                         @if (count($jobs))
                             @foreach ($jobs as $job)
@@ -34,15 +55,31 @@
                                     <div class="font-normal text-gray-600 text-md px-1">{{ $job->started_at }} - {{ $job->quitted_at }}</div>
                                 </div>
                             @endforeach
-                        @else 
-                            No working experience information entered yet...
+                        @else
+                            <div class="h-16">
+                                No working experience information entered yet...
+                            </div>
                         @endif
+                        {{-- @auth
+                            <div class="mt-2 py-2 px-4 border-t border-gray-200 absolute inset-x-0 bottom-0">
+                                <a href="/about/job/create" class="text-sm text-blue-400 hover:underline">Add working experience</a>
+                            </div>
+                        @endauth --}}
                     </div>                
                 </div>
             </div>
             <div class="pt-2 h-full">
                 <div class="p-4 text-gray-700 rounded bg-white h-full">
-                    <div class="text-2xl font-semibold select-none">Education</div>
+                    <div class="flex select-none mb-2 flex items-center justify-between">
+                        <div class="text-2xl font-semibold">
+                            <i class="fas fa-graduation-cap"></i> Education
+                        </div>
+                        @auth
+                            <div class="text-sm">
+                                <a href="/about/job/create" class="text-sm text-blue-400 hover:underline">Add</a>
+                            </div>
+                        @endauth
+                    </div>
                     <div class="educations-list flex flex-col">
                         @if (count($educations))
                             @foreach ($educations as $education)
@@ -58,9 +95,16 @@
                                     @endif
                                 </div>
                             @endforeach
-                        @else 
-                            No educational information entered yet...
+                        @else
+                            <div class="h-16">
+                                No educational information entered yet...
+                            </div>
                         @endif
+                        {{-- @auth
+                            <div class="mt-2 py-2 px-4 border-t border-gray-200 absolute inset-x-0 bottom-0">
+                                <a href="/about/education/create" class="text-sm text-blue-400 hover:underline">Add educational information</a>
+                            </div>
+                        @endauth --}}
                     </div>                         
                 </div>
             </div>
