1ccab6db5b2f8a876d99164a9c72a17609fe1285
Emre UYGUR <60958122+dogo-emre@users.noreply.github.com>
2020-06-25 16:08:11 +0300

    Update login.blade.php
    *Page will show errors when credentials are invalid instead of firing 419 page
    *Better responsive design

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,14 +7,24 @@
 @section('layoutContent')
     <form action="{{ route("login") }}" method="POST">
         @csrf
-        <div class="flex flex-col content-center p-6 w-1/3 m-auto border-2 border-gray-300 rounded">
-            <div class="flex flex-col m-2">
+        <div class="flex flex-col content-center p-6 w-full sm:w-1/2 lg:w-1/3 m-auto border-2 border-gray-300 rounded">
+            <div class="flex flex-col mx-2">
                 <label>Email</label>
-                <input type="email" class="border rounded border-gray-200 p-1 focus:border-blue-200" name="email">
+                <input type="email" value="{{ old('email')}}" class="border rounded border-gray-200 p-1 mb-2 focus:border-blue-200" name="email">
+                @error('email')
+                    <span class="text-sm text-red-500" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
             </div>
-            <div class="flex flex-col m-2">
+            <div class="flex flex-col mx-2">
                 <label>Password</label>
-                <input type="password" class="border rounded border-gray-200 p-1 focus:border-blue-200" name="password">
+                <input type="password" class="border rounded border-gray-200 p-1 mb-2 focus:border-blue-200" name="password">
+                @error('password')
+                    <span class="text-sm text-red-500" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
             </div>
             <div class="m-2">
                 <button type="submit" class="px-2 py-1 bg-blue-600 text-white rounded focus:border-blue-200">Login</button>
