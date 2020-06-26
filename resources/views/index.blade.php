55dee194af16c326c068c370b582ea85ebcccde4
Emre UYGUR <60958122+dogo-emre@users.noreply.github.com>
2020-06-26 23:54:23 +0300

    Update index.blade.php
    config helper will get the name

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,6 +6,6 @@
 
 @section('layoutContent')
     <img id="bg-homepage" src="{{ asset('/images/programming.gif') }}" alt="programming">
-    <div class="text-2xl text-gray-700 font-semibold">Emre UYGUR</div>
+    <div class="text-2xl text-gray-700 font-semibold">{{ config("app.name") }}</div>
     <div class="mt-1 text-lg text-gray-700 font-semibold">Computer Engineer</div>
 @endsection
