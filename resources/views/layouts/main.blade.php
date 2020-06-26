6ff46fbaa330dbf7d1690213f4927010c224a546
Emre UYGUR <60958122+dogo-emre@users.noreply.github.com>
2020-06-26 22:35:39 +0300

    BugFix

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,7 +5,7 @@
 @endsection
 
 @section('content')
-<Navbar @auth {{ __('is-logged') }} name="{{ config("app.name") }}" @endauth></Navbar>
+<Navbar @auth {{ __('is-logged') }} @endauth name="{{ config("app.name") }}"></Navbar>
     <div class="mx-8 mb-8 mt-24 sm:m-16">
         @yield('layoutContent')
     </div>
